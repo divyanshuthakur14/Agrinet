655b41f3fb75e24c070ee365d53992a09375b403
divyanshuthakur14 <singhthakurdivyanshu@gmail.com>
2024-08-25 23:01:36 +0530

    login & registration page 4

---
+++ app/src/main/java/com/example/agrinet/Login.java
@@ -0,0 +1,60 @@
+package com.example.agrinet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+
+    private boolean passwordShowing = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final EditText usernameET = findViewById(R.id.usernameET);
+        final EditText passwordET = findViewById(R.id.passwordET);
+        final ImageView passwordIcon = findViewById(R.id.passwordIcon);
+        final TextView signUpBtn = findViewById(R.id.signUpBtn);
+
+        passwordIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(passwordShowing){
+                        passwordShowing = false;
+
+                        passwordET.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        passwordIcon.setImageResource(R.drawable.baseline_visibility_24);
+            }
+                        else{
+                    passwordShowing = true;
+
+                    passwordET.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    passwordIcon.setImageResource(R.drawable.baseline_visibility_24);
+                }
+
+            }
+        });
+
+       signUpBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               startActivity(new Intent(Login.this, Register.class));
+           }
+       });
+    }
+}
+
+
+
