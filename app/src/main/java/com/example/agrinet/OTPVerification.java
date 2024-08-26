8c7ce6f05cfaba72d815997a8306b55d8b7cb41a
divyanshuthakur14 <singhthakurdivyanshu@gmail.com>
2024-08-26 23:02:10 +0530

    login & registration page 5

--- app/src/main/java/com/example/agrinet/OTPVerification.java
+++ app/src/main/java/com/example/agrinet/OTPVerification.java
@@ -1,6 +1,11 @@
 package com.example.agrinet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,38 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class OTPVerification extends AppCompatActivity {
 
+    EditText enternumber;
+    Button getotpbutton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_otpverification);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        enternumber = findViewById(R.id.input_mobile_number);
+        getotpbutton= findViewById(R.id.buttongetotp);
+
+        getotpbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!enternumber.getText().toString().trim().isEmpty()){
+                    if(enternumber.getText().toString().trim().length() ==10){
+
+                        Intent intent = new Intent(getApplicationContext(), VerifyOTP.class);
+                        intent.putExtra("mobile",enternumber.getText().toString());
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(OTPVerification.this,"Please enter correct number",Toast.LENGTH_SHORT).show();
+                    }}else{
+
+                        Toast.makeText(OTPVerification.this, "Enter mobile number", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+
         });
+
     }
 }
---
+++ app/src/main/java/com/example/agrinet/VerifyOTP.java
@@ -0,0 +1,162 @@
+package com.example.agrinet;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class VerifyOTP extends AppCompatActivity {
+
+    EditText inputnumber1,inputnumber2,inputnumber3,inputnumber4,inputnumber5,inputnumber6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_otp);
+
+        Button verifybuttonclick = findViewById(R.id.buttongetotp);
+
+        inputnumber1 = findViewById(R.id.inputotp1);
+        inputnumber2 = findViewById(R.id.inputotp2);
+        inputnumber3 = findViewById(R.id.inputotp3);
+        inputnumber4 = findViewById(R.id.inputotp4);
+        inputnumber5 = findViewById(R.id.inputotp5);
+        inputnumber6 = findViewById(R.id.inputotp6);
+
+        TextView textView = findViewById(R.id.textmobileshownumber);
+        textView.setText(String.format(
+                "+91-%s", getIntent().getStringExtra("mobile")
+        ));
+
+        verifybuttonclick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!inputnumber1.getText().toString().trim().isEmpty() && !inputnumber2.getText().toString().trim().isEmpty() && !inputnumber3.getText().toString().trim().isEmpty() && !inputnumber4.getText().toString().trim().isEmpty() && !inputnumber5.getText().toString().trim().isEmpty() && !inputnumber6.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(VerifyOTP.this, "otp verify", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(VerifyOTP.this, "please enter all number", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        numberotpmove();
+
+    }
+
+    private void numberotpmove(){
+
+        inputnumber1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if(!s.toString().trim().isEmpty()){
+                    inputnumber2.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputnumber2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if(!s.toString().trim().isEmpty()){
+                    inputnumber3.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputnumber3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if(!s.toString().trim().isEmpty()){
+                    inputnumber4.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputnumber4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if(!s.toString().trim().isEmpty()){
+                    inputnumber5.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputnumber5.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if(!s.toString().trim().isEmpty()){
+                    inputnumber6.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        }
+    }
